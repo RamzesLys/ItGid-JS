6d681b6015470bffb6a437b66d570d82d31bd68b
RamzesLys <ramzes311982@gmail.com>
2023-03-10 20:34:08 +0200

    Lesson 2+

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log(4-6);
+
+// document.getElementById('out').innerHTML = 777;
+
+// out.innerHTML = '<i>Hi</i>';
+// out.innerText = '<i>Hi</i>';
+
+let p;
+p = document.getElementById('out');
+d = document.querySelector('#out')
+p.innerHTML = 555;
+d.innerHTML = 777777777
+console.log(p);
