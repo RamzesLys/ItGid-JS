ca248b7faa5759dac2aaa06e22cc83934893c1fc
RamzesLys <ramzes311982@gmail.com>
2023-03-12 20:00:23 +0200

    if else

--- lesson4.js
+++ lesson4.js
@@ -1,10 +1,44 @@
-let a = 9;
-let b = '5'
+// let a = -5;
+// let b = 5
 
-if (b == 5) {
-  console.log('yes');
-} else {
-  console.log('no');
-};
+// if (a > 0 && a <= 20) {
+//   console.log('1');
+// } else if (a > 20 && a < 30) {
+//   console.log('2');
+// } else if (a > 30 && a <= 40) {
+//   console.log('3');
+// } else {
+//   console.log('Не существует');
+// }
 
-console.log('My iPad');
+// function randomInteger(min, max) {
+//   let rand = min - 0.5 + Math.random() * (max - min +1);
+//   rand = Math.round(rand);
+//   return rand;
+// }
+// randomInteger();
+
+let r = Math.round(Math.random() * 10);
+console.log(r);
+
+let userNum = document.querySelector('#user-num');
+
+document.querySelector('#btn').onclick = checkNum;
+
+function checkNum() {
+  let num = userNum.value;
+  num = parseInt(num);
+  console.log(num);
+  
+  if (isNaN(num)) {
+    alert('Введіть число');
+  }
+  else {
+  if (num == r) {
+    alert('Вірно');
+  }
+  else {
+    alert('Спробуй ще');
+  }
+  }
+}
