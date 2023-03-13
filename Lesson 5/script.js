92c244c8949a1718622832410a3e7857593e5539
RamzesLys <ramzes311982@gmail.com>
2023-03-13 16:03:42 +0200

    Lesson 5

--- Lesson 5/script.js
+++ Lesson 5/script.js
@@ -1,1 +1,2 @@
-document.querySelector('p').innerHTML += ' bye'
+document.querySelector('p').innerHTML += ' Приклад span <b>Hi</b>';
+document.querySelector('p').innerText += ' Приклад span <b>Hi</b>';
