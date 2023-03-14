003a7e9c1cc80ad2f50b2259f9bda25ed0681a46
RamzesLys <ramzes311982@gmail.com>
2023-03-14 18:38:03 +0200

    Lesson 6

---
+++ Lesson 6/script.js
@@ -0,0 +1,8 @@
+document.querySelector('.push').onclick = function () {
+  console.log(document.querySelector('.text-input').value);
+  console.log(document.querySelector('.pass-input').value);
+  console.log(document.querySelector('.range-input').value);
+  console.log(document.querySelector('.date-input').value);
+  console.log(document.querySelector('.color-input').value);
+  console.log(document.querySelector('#checkbox-input').checked);
+}
