8ec97254da0189634740a943687a2a95723d5e57
RamzesLys <ramzes311982@gmail.com>
2023-03-15 22:00:28 +0200

    Lesson 9 task _

--- Lesson 9 (keyboard)/script.js
+++ Lesson 9 (keyboard)/script.js
@@ -1,1 +1,21 @@
-console.log('Hello');
+const test = document.querySelector('#test');
+test.onkeypress = (e) => {
+  // console.log(e.keyCode);
+  
+  if (e.keyCode < 97 || e.keyCode > 122) {
+    console.log('Невірні символи');
+    return false
+    
+  } else {
+    let upperKey = e.key.toUpperCase();
+    e.key.innerHTML += upperKey;
+    console.log(upperKey);
+    return upperKey
+
+    
+
+  }
+
+}
+
+
