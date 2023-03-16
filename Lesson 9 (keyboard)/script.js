547bb4c4399f9e814bb6d50b697c98a2ed6aacc3
RamzesLys <ramzes311982@gmail.com>
2023-03-16 19:28:08 +0200

    Lesson 9 Task +

--- Lesson 9 (keyboard)/script.js
+++ Lesson 9 (keyboard)/script.js
@@ -1,17 +1,16 @@
 const input = document.querySelector('#input');
 
-input.onkeypress = (e) => {
-  if (e.keyCode < 97 || e.keyCode > 122) {
-    console.log('Введіть літери');
-    return false
+// input.onkeypress = (e) => {
+//   if (e.keyCode < 97 || e.keyCode > 122) {
+//     console.log('Введіть літери');
+//     return false
     
-  } else {
-    let upperKey = e.key.toUpperCase();
-    e.value += upperKey;
-    console.log(upperKey);
-    return upperKey
-  }
+//   } else {
+//     input.value = input.value.toUpperCase()
+//   }
 
-}
+// }
 
-
+input.addEventListener('input', () => {
+  input.value = input.value.toUpperCase();
+})
