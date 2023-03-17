afb836d1f67eea4768bd92cfae47031504103530
RamzesLys <ramzes311982@gmail.com>
2023-03-17 17:41:36 +0200

    Lesson 8-2

--- Lesson 8-2 (Tabs)/script.js
+++ Lesson 8-2 (Tabs)/script.js
@@ -1,1 +1,8 @@
-console.log('Hi');
+const init = () => {
+ let tabBody = document.querySelectorAll('.tab-body');
+ for (let i = 1; i < tabBody.length; i++) {
+  tabBody[i].style.display = "none"
+ } 
+}
+
+init()
