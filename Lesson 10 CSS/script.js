1de89d6cee5d2e4d302289a51923fbf4902c3285
RamzesLys <ramzes311982@gmail.com>
2023-03-17 11:35:19 +0200

    Lesson 10 +

---
+++ Lesson 10 CSS/script.js
@@ -0,0 +1,18 @@
+let block1 = document.querySelector('.block-1');
+let block1Style = block1.style;
+block1.style.width = '100px'
+
+block1.classList.add('red');
+block1.classList.add('green');
+block1.classList.remove('green');
+console.log(block1.classList); // список усіх класів
+
+console.log(block1.classList.contains('red')); // перевірка наявності класів в елемента 
+
+block1.classList.toggle('red');
+block1.classList.toggle('red');
+
+const button = document.querySelector('.button');
+button.onclick = () => {
+  block1.classList.toggle('red');
+}
