d0e255cbf53dd034cc8d46305ac786cbf5329b84
RamzesLys <ramzes311982@gmail.com>
2023-03-18 23:39:12 +0200

    Lesson 11-1 +

--- Lesson 11-1/js/modal.js
+++ Lesson 11-1/js/modal.js
@@ -7,14 +7,39 @@ document.querySelectorAll('.modal-close').forEach(function (element) {
   element.onclick = closeModal;
 });
 
+document.querySelectorAll('.modal-wrap').forEach(function (element) {
+ element.onclick = closeModalWrap;
+});
+
+
+
 function showModal() {
   let modalId = this.dataset.modal;
   document.querySelector(modalId).parentElement.classList.remove('hide');
   document.querySelector(modalId).classList.remove('hide');
+  document.onkeydown = function (event) {
+   if (event.keyCode == 27) {
+      document.querySelectorAll('.modal-wrap').forEach(function (element) {
+        element.classList.add('hide');
+        element.children[0].classList.add('hide');
+        document.onkeydown = null;
+      })
+    }
+  }
 }
 
 function closeModal() {
   let modalId = this.dataset.modal;
   document.querySelector(modalId).parentElement.classList.add('hide');
   document.querySelector(modalId).classList.add('hide');
+  document.onkeydown = null;
 }
+
+
+function closeModalWrap() {
+  this.classList.add('hide');
+ this.children[0].classList.add('hide');
+ document.onkeydown = null;
+}
+
+
