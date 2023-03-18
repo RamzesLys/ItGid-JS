ea96401f95b9aeb93ec000640036276c3fda971a
RamzesLys <ramzes311982@gmail.com>
2023-03-18 10:24:44 +0200

    Lesson 8-2 +

--- Lesson 8-2 (Tabs)/script.js
+++ Lesson 8-2 (Tabs)/script.js
@@ -1,8 +1,23 @@
 const init = () => {
  let tabBody = document.querySelectorAll('.tab-body');
  for (let i = 1; i < tabBody.length; i++) {
-  tabBody[i].style.display = "none"
+  tabBody[i].style.display = "none";
  } 
 }
 
-init()
+init();
+
+function showTabs() {
+  let data = this.getAttribute('data');
+  let tabBody = document.querySelectorAll('.tab-body');
+  for (i = 0; i< tab.length; i ++) {
+    tabBody[i].style.display = "none";
+  }
+  document.querySelector(`.tab-body[data="${data}"]`).style.display = 'block';
+}
+
+let tab = document.querySelectorAll('.tab');
+tab.forEach((element) => {
+  element.onclick = showTabs;
+});
+
