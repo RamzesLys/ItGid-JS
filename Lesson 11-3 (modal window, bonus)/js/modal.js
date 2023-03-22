c9c3a68c1d3b4cebc70c5e672b8044efbfea6a84
RamzesLys <ramzes311982@gmail.com>
2023-03-22 13:40:06 +0200

    Lesson 11-3 +

--- Lesson 11-3 (modal window, bonus)/js/modal.js
+++ Lesson 11-3 (modal window, bonus)/js/modal.js
@@ -7,28 +7,29 @@ document.querySelectorAll('.modal-close').forEach(function (element) {
 });
 
 document.querySelectorAll('.modal-wrap').forEach(function (element) {
-  //console.log(element)
+  // console.log(element)
  element.onclick = closeModalWrap;
 });
-
 
 function showModal() {
   let modalId = this.dataset.modal;
   document.querySelector(modalId).classList.remove('hide');
-  // document.onkeydown = function (event) {
-  //  if (event.keyCode == 27) {
-  //     document.querySelectorAll('.modal-wrap').forEach(function (element) {
-  //       element.classList.add('hide');
-  //       document.onkeydown = null;
-  //     });
-  //   }
-  // }
+  document.onkeydown = function (event) {
+
+   if (event.code == 'Escape') {
+      document.querySelectorAll('.modal-wrap').forEach(function (element) {
+        element.classList.add('hide');
+        document.onkeydown = null;
+      });
+      
+    }
+  }
 }
 
 function closeModal() {
   let modalId = this.dataset.modal;
   document.querySelector(modalId).classList.add('hide');
-  document.onkeydown = null;
+ document.onkeydown = null;
 }
 
 function closeModalWrap() {
