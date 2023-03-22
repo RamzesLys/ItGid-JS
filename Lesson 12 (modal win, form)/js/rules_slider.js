f7dac19ce54e0ff159c975c012ad01400e38e1d1
RamzesLys <ramzes311982@gmail.com>
2023-03-22 20:01:16 +0200

    Lesson 12 Task +

--- Lesson 12 (modal win, form)/js/rules_slider.js
+++ Lesson 12 (modal win, form)/js/rules_slider.js
@@ -2,12 +2,7 @@ document.querySelector('.read-rules').onclick = () => {
   document.querySelector('.form-slider').style.marginLeft = '-250px'; 
 }
 
-const back = document.querySelectorAll('.read-rules-back');
-// back.onclick = () => {
-//   document.querySelector('.form-slider').style.marginLeft = '0'; 
-// }
-
-back.forEach((element) => {
+document.querySelectorAll('.read-rules-back').forEach((element) => {
   element.onclick = () => {
     document.querySelector('.form-slider').style.marginLeft = '0';
   }
