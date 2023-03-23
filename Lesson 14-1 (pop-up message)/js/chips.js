6cec9a206830d2fc8bcbb4136dd6b632ce6b65a2
RamzesLys <ramzes311982@gmail.com>
2023-03-23 16:44:01 +0200

    Lesson 14-1 task +

--- Lesson 14-1 (pop-up message)/js/chips.js
+++ Lesson 14-1 (pop-up message)/js/chips.js
@@ -1,14 +1,37 @@
-const chips = (message) => {
+const chips = (message, timeremove = 3000) => {
+  //if (timeremove === undefined) timeremove = 3000;
   let chips = document.createElement('div');
   chips.classList.add('chips');
   chips.innerHTML = message;
-  console.log(chips);
-  document.querySelector('body').appendChild(chips)
+  chips.style.backgroundColor = 'red'
+  addChips(chips)
+  setTimeout(() => {deleteChips(chips)}, timeremove);
+}
+
+const deleteChips = (chips) => {
+  chips.remove();
+  let allChips = document.querySelectorAll('.chips-field .chips');
+  if (allChips.length == 0) document.querySelector('.chips-field').remove();
+}
+
+const addChips = (chips) => {
+  let chipsField = document.querySelector('.chips-field');
+  if (chipsField) {
+    chipsField.appendChild(chips);
+  } 
+  else {
+    let chipsField = document.createElement('div');
+    chipsField.classList.add('chips-field');
+    document.querySelector('body').appendChild(chipsField);
+    chipsField.appendChild(chips);
+  }
 }
 
 document.querySelector('button').onclick = () => {
-  chips('Привіт')
+  chips('Привіт', 2500)
 };
+
+
 // chips('hello');
 
 
