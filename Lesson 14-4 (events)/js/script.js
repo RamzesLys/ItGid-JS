98de97dd33d36c5d338e07a060b0e9e2b61f7fce
RamzesLys <ramzes311982@gmail.com>
2023-04-06 14:16:20 +0300

    Lesson 14-4

--- Lesson 14-4 (events)/js/script.js
+++ Lesson 14-4 (events)/js/script.js
@@ -12,7 +12,17 @@ const myClick = (event) => {
 // document.querySelector('html').onclick = myClick;
 
 document.onclick = (event) => {
-  console.log(event);
+  //console.log(event);
   if (event.target.id == 'one')
   console.log('Клік на one');
+}
+
+document.querySelector('.two').onclick = (e) => {
+  e.stopPropagation();
+  console.log('Клік на tw0');
+}
+
+document.querySelector('.three').onclick = function () {
+  this.style.display = 'none';
+  console.log('Клік на 3');
 }
