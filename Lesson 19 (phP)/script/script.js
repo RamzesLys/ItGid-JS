0081c7d3eef5fa7df9afa25c3c58dfaf32a5c545
RamzesLys <ramzes311982@gmail.com>
2023-05-15 13:04:14 +0300

    Lesson 19 + correct

--- Lesson 19 (phP)/script/script.js
+++ Lesson 19 (phP)/script/script.js
@@ -1,12 +1,11 @@
-document.querySelector('#signup-submit').onclick = (event) => {
+document.querySelector('#signup-submit').onclick = function(event) {
   event.preventDefault();
-  console.log('workkkk');
   let name = document.querySelector('#signup-name').value;
   let pass = document.querySelector('#signup-pass').value;
   let email = document.querySelector('#signup-email').value;
-  let birhDay = document.querySelector('#signup-birthday').value;
+  let birthday = document.querySelector('#signup-birthday').value;
   let sex = document.querySelectorAll('.sex');
-  for (let i = 0; i < sex.length; i++) {
+  for (let i = 0; i<sex.length; i++) {
     if (sex[i].checked) {
       sex = sex[i].value;
       break
@@ -16,25 +15,42 @@ document.querySelector('#signup-submit').onclick = (event) => {
     "name": name,
     "pass": pass,
     "email": email,
-    "birthday": birhDay,
-    "sex": sex
+    "birthday": birthday,
+    "sex": sex,
   }
-ajax('core/signup.php', 'POST', login, data);
+  ajax('core/signup.php', 'POST', signup, data)
 
-  function login(result) {
+  function signup(result) {
     console.log(result);
     if (result == 2) {
-      alert('Заповніть поля')
-    }
-    else if (result == 1) {
-      alert('Успішно')
+      alert('Заповніть поля');
+    } else if (result == 1) {
+      alert('Успішно');
     } else {
-      alert('Повторіть реєстрацію')
+      alert('Спробуйте пізніше');
     }
   }
 }
 
+document.querySelector('#login-submit').onclick = function(event) {
+  event.preventDefault();
+  let email = document.querySelector('#login-email').value;
+  let pass = document.querySelector('#login-pass').value;
+ 
+  let data = {
+    "email": email,
+    "pass": pass,
+  }
+  ajax('core/login.php', 'POST', login, data)
 
-
-
-
+  function login(result) {
+    console.log(result);
+    if (result == 2) {
+      alert('Заповніть поля');
+    } else if (result == 0) {
+      alert('Користувач не знайдений');
+    } else {
+      console.log(result);
+    }
+  }
+}
