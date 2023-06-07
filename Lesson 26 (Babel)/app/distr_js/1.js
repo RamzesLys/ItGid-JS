02156ce55249e9f37d65f00247fc5f88746f6603
RamzesLys <ramzes311982@gmail.com>
2023-06-07 17:42:41 +0300

    Lesson 27 complete

---
+++ Lesson 26 (Babel)/app/distr_js/1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var a = 7;
+console.log(a);
+b = 'hello ' + a + ' hi';
+
+var c = function c() {
+  console.log('Hi, Ramzes');
+};
---
+++ Lesson 26 (Babel)/app/src_js/1.js
@@ -0,0 +1,8 @@
+let a = 7;
+console.log(a);
+b = `hello ${a} hi`;
+
+
+let c = () => {
+  console.log('Hi, Ramzes');
+}
